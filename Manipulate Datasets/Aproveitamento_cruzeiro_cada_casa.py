ca7993ff7973cdb161159ae44d1732a9080b3142
antoniov-01 <antonio.v@ges.inatel.br>
2023-07-25 22:03:27 -0300

    Aproveitamento do cruzeiro em cada casa

--- Manipulate Datasets/Aproveitamento_cruzeiro_cada_casa.py
+++ Manipulate Datasets/Aproveitamento_cruzeiro_cada_casa.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import json
-import os
 
 # MANIPULANDO O DATASET DE MANDANTE DO CRUZEIRO E VERIFICANDO SEU DESEMPENHO EM CADA ESTADIO
 
